a664793940e6af3ded2c36a45ec0116455911287
David de Koning <david.dekoning@arup.com>
2018-07-02 14:59:58 -0400

    Add reload function for development

--- sketchup-tools_loader.rb
+++ sketchup-tools_loader.rb
@@ -3,6 +3,18 @@ require "extensions.rb"
 
 # Load plugin as extension (so that user can disable it)
 
+module DKS
+    def self.reload
+        original_verbose = $VERBOSE
+        $VERBOSE = nil
+        pattern = File.join(__dir__, '**/*.rb')
+        Dir.glob(pattern).each { |file| load file }.size
+        # Cannot use `Sketchup.load` because its an alias for `Sketchup.require`.
+    ensure
+        $VERBOSE = original_verbose
+    end
+end
+
 cutlist_loader = SketchupExtension.new "My_Plugin Loader", "cutlist/cutlist.rb"
 cutlist_loader.copyright= "Copyright 2017-2018 David de Konign"
 cutlist_loader.creator= "David de Koning"
